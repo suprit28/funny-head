2c95206b41fb3528d8d9dd078afd4706239373dc
SUPRIT CHAUDHARI <supritchaudhari.iitg@gmail.com>
2022-07-20 15:12:04 +0530

    initial commit

---
+++ src/main/java/com/springboot/funnyhead/FunnyHeadApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.funnyhead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunnyHeadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunnyHeadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/funnyhead/FunnyHeadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.funnyhead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunnyHeadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
